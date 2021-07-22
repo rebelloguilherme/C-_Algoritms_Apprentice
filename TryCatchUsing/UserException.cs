51d6afd6fe93c933f60042f4527b66e0725ac856
rebelloguilherme <32513078+rebelloguilherme@users.noreply.github.com>
2021-07-21 22:12:47 -0300

    Example of User throwing an exception

---
+++ TryCatchUsing/UserException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchUsing
+{
+
+    class Area
+    {
+        public static double Quad(double bas, double alt)
+        {
+            if (bas == 0.0 || alt == 0.0)
+            {
+                throw new Exception("Base ou altura não podem ser igual a zero");
+            }
+            return bas * alt;
+        }
+    }
+
+
+
+    class UserException
+    {
+        static void Main(string[] args)
+        {
+            double area = 0.0;
+            try
+            {
+                area = Area.Quad(15.0, 20.0); // if some parameter is 0, will be thrown an exception by user previously coded..
+                Console.WriteLine("Area do quadrado é: {0}", area);
+            }
+            catch(Exception e)// or DivideByZeroException for more specific exception
+            {
+                Console.WriteLine("Erro!");
+                Console.WriteLine("Conteudo do e:"+e);
+                Console.WriteLine("Conteudo do e.Message: "+ e.Message);
+                Console.WriteLine("Conteudo do e.GetType(): " + e.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("Fim do processo, sempre executa!");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
