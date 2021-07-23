2f8cdbe6f88ea253bb12d757d68128229fd7e3fa
rebelloguilherme <32513078+rebelloguilherme@users.noreply.github.com>
2021-07-23 11:26:18 -0300

    Enum and List Usage
    Creating a list of cards, using Enum for suits.
    Populating a deck list with valid cards as well.

---
+++ EnumUsageAndListAdding.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumUsing
+{
+
+    public enum Suit { heart, diamond, club, spade };
+
+    public class Card
+    {
+        public Card()
+        {
+        }
+        
+        public Suit Suit { get; set; }
+        public int Value { get; set; }
+
+        public Card(Suit suit, int value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+    }
+
+    class Program
+    {
+        
+        static void Main()
+        {
+
+            List<Card> deck = new List<Card>();
+
+            for (int x=0; x<4; x++)
+            {
+                for (int y=1; y<14; y++)
+                {
+                    deck.Add(new Card((Suit)x, y)); //populando o deck com as cartas                                   
+                }
+            }
+
+
+            void showDeck()
+            {
+                Console.WriteLine("Número total de cartas no deck: " + deck.Count());
+                foreach (Card c in deck)
+                {
+                    Console.WriteLine("Carta: {0} of {1}", c.Value, c.Suit);
+                }
+
+            }
+
+            showDeck();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
